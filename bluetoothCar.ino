4c67d2c939a3854974d30daa812e4cab20b7197a
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-08-06 01:44:40 +0700

    Update bluetoothCar.ino

--- bluetoothCar.ino
+++ bluetoothCar.ino
@@ -11,3 +11,13 @@ AF_DCMotor motor3(3, MOTOR34_1KHZ);
 AF_DCMotor motor4(4, MOTOR34_1KHZ);
 
 char val;
+
+void setup()
+{
+  Serial.begin(115200);
+  SerialBT.begin(9600);   // baud rate untuk module bluetooth
+  motor1.setSpeed(255);   // atur kecepatan motor
+  motor2.setSpeed(255);   // atur kecepatan motor
+  motor3.setSpeed(255);   // atur kecepatan motor
+  motor4.setSpeed(255);   // atur kecepatan motor
+}
