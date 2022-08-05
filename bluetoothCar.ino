cd8a395b6288c91ab2290ebe1e137e665ed5666d
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-08-06 01:43:07 +0700

    Update bluetoothCar.ino

--- bluetoothCar.ino
+++ bluetoothCar.ino
@@ -3,3 +3,9 @@
 #define BTTX 9
 #define BTRX 10
 SoftwareSerial SerialBT(BTTX, BTRX);
+
+// inisialisasi motor
+AF_DCMotor motor1(1, MOTOR12_1KHZ);
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
