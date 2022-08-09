c657e12371c2d360b0c35814ca4a89cd01590fc9
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-08-09 23:44:21 +0700

    Update bluetoothCar2.ino

--- bluetoothCar2.ino
+++ bluetoothCar2.ino
@@ -10,3 +10,13 @@ AF_DCMotor motor3(3, MOTOR34_1KHZ);
 AF_DCMotor motor4(4, MOTOR34_1KHZ);
 
 char val;
+
+void setup()
+{
+  Serial.begin(115200);
+  SerialBT.begin(9600);
+  motor1.setSpeed(255);
+  motor2.setSpeed(255);
+  motor3.setSpeed(255);
+  motor4.setSpeed(255);
+}
